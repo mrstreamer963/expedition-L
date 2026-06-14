f84417546f8857bf4836aaed979e8484154f1d6d
mrstreamer963 <698lo7epy@mozmail.com>
2026-06-14 20:06:09 +0300

    Remove Vec2 from public exports

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,7 @@
 import { GameWorld } from './gameWorld'
 import { GameServer, SaveData } from './types'
 
-export type { GameServer, ClientSnapshot, PlayerAction, SaveData, Vec2 } from './types'
+export type { GameServer, ClientSnapshot, PlayerAction, SaveData } from './types'
 
 export function createGameServer(savedState?: SaveData): GameServer {
   return new GameWorld(savedState)
